ecee521a6dbcde40542c6ebc3be6c11395d7fac3
rafaelangel85 <34768304+rafaelangel85@users.noreply.github.com>
2022-04-06 14:44:48 +0200

    añadido app chat que esta bien perrota

--- example_dev_to/main.py
+++ example_dev_to/main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivymd.uix.list import OneLineIconListItem, IconLeftWidget
 
+
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Purple"
@@ -8,8 +9,8 @@ class MainApp(MDApp):
     def add_item(self, text):
         new_list_item = OneLineIconListItem(text=text)
         new_list_item.add_widget(
-            IconLeftWidget(icon = "language-python")
-            )
+            IconLeftWidget(icon="language-python")
+        )
         self.root.ids.listcontainer.add_widget(new_list_item)
         self.root.ids.listinput.text = ''
 
--- main.py
+++ main.py
@@ -1,113 +1,33 @@
-from kivy.app import App
-from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from database import DataBase
+from main_kv import screen_helper
 
 
-class CreateAccountWindow(Screen):
-    namee = ObjectProperty(None)
-    email = ObjectProperty(None)
-    password = ObjectProperty(None)
-
-    def submit(self):
-        if self.namee.text != "" and self.email.text != "" and self.email.text.count(
-                "@") == 1 and self.email.text.count(".") > 0:
-            if self.password != "":
-                db.add_user(self.email.text, self.password.text, self.namee.text)
-
-                self.reset()
-
-                sm.current = "login"
-            else:
-                invalidForm()
-        else:
-            invalidForm()
-
-    def login(self):
-        self.reset()
-        sm.current = "login"
-
-    def reset(self):
-        self.email.text = ""
-        self.password.text = ""
-        self.namee.text = ""
-
-
-class LoginWindow(Screen):
-    email = ObjectProperty(None)
-    password = ObjectProperty(None)
-
-    def loginBtn(self):
-        if db.validate(self.email.text, self.password.text):
-            MainWindow.current = self.email.text
-            self.reset()
-            sm.current = "main"
-        else:
-            invalidLogin()
-
-    def createBtn(self):
-        self.reset()
-        sm.current = "create"
-
-    def reset(self):
-        self.email.text = ""
-        self.password.text = ""
-
-
-class MainWindow(Screen):
-    n = ObjectProperty(None)
-    created = ObjectProperty(None)
-    email = ObjectProperty(None)
-    current = ""
-
-    def logOut(self):
-        sm.current = "login"
-
-    def on_enter(self, *args):
-        password, name, created = db.get_user(self.current)
-        self.n.text = "Account Name: " + name
-        self.email.text = "Email: " + self.current
-        self.created.text = "Created On: " + created
-
-
-class WindowManager(ScreenManager):
+class MenuScreen(Screen):
     pass
 
 
-def invalidLogin():
-    pop = Popup(title='Invalid Login',
-                content=Label(text='Invalid username or password.'),
-                size_hint=(None, None), size=(400, 400))
-    pop.open()
+class ProfileScreen(Screen):
+    pass
 
 
-def invalidForm():
-    pop = Popup(title='Invalid Form',
-                content=Label(text='Please fill in all inputs with valid information.'),
-                size_hint=(None, None), size=(400, 400))
-
-    pop.open()
+class UploadScreen(Screen):
+    pass
 
 
-kv = Builder.load_file("my.kv")
-
-sm = WindowManager()
-db = DataBase("users.txt")
-
-screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"), MainWindow(name="main")]
-for screen in screens:
-    sm.add_widget(screen)
-
-sm.current = "login"
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(ProfileScreen(name='profile'))
+sm.add_widget(UploadScreen(name='upload'))
 
 
-class MyMainApp(App):
+class MainApp(MDApp):
+
     def build(self):
-        return sm
+        screen = Builder.load_string(screen_helper)
+        return screen
 
 
-if __name__ == "__main__":
-    MyMainApp().run()
+MainApp().run()
