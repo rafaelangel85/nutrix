94d3767ef54633ae1c0100a0c7d78c4e19cec3b7
rafaelangel85 <34768304+rafaelangel85@users.noreply.github.com>
2022-04-20 10:40:06 +0200

    Update main_screen.py

--- pyNutrix/View/MainScreen/main_screen.py
+++ pyNutrix/View/MainScreen/main_screen.py
@@ -1,10 +1,6 @@
 from typing import NoReturn
 from kivy.app import App
-from kivy.properties import StringProperty
-
 from View.base_screen import BaseScreenView
-from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.tab import MDTabsBase
 
 __version__ = '0.0.3'
 
@@ -24,8 +20,3 @@ class MainScreenView(BaseScreenView):
 
     def get_version(self) -> str:
         return __version__
-
-
-class MainThreeTab(MDFloatLayout, MDTabsBase):
-    """Class implementing content for a tab."""
-    content_text = StringProperty("")
