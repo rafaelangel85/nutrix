e60ff99466df765ce50f7489511d45e1476551c8
rafaelangel85 <34768304+rafaelangel85@users.noreply.github.com>
2022-04-22 08:00:43 +0200

    Ejemplo a cargo cultear

---
+++ pyNutrix/Utility/examples/custom_list_item.py
@@ -0,0 +1,53 @@
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+from kivymd.app import MDApp
+from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.icon_definitions import md_icons
+
+
+KV = '''
+<ListItemWithCheckbox>:
+
+    IconLeftWidget:
+        icon: root.icon
+
+    RightCheckbox:
+
+
+MDBoxLayout:
+
+    ScrollView:
+
+        MDList:
+            id: scroll
+'''
+
+
+class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    """Custom list item."""
+
+    icon = StringProperty("android")
+
+
+class RightCheckbox(IRightBodyTouch, MDCheckbox):
+    """Custom right container."""
+
+
+class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.theme_cls.primary_palette = "Teal"
+        self.theme_cls.primary_hue = "500"
+
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        icons = list(md_icons.keys())
+        for i in range(30):
+            self.root.ids.scroll.add_widget(ListItemWithCheckbox(text=f"Item {i}", icon=icons[i]))
+
+
+MainApp().run()
